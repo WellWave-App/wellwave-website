02b752278e2eb8011a1ceb185d24119304fff51f
ThitarThitaporn <88717990+ThitarThitaporn@users.noreply.github.com>
2025-02-05 14:11:04 +0700

    navbar : edit color border

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,14 +6,14 @@ import React from 'react';
 import { usePathname } from 'next/navigation';
 
 const Navbar: React.FC = () => {
-  const pathname = usePathname(); // ดึงเส้นทางปัจจุบัน
+  const pathname = usePathname();
 
   return (
     <nav className="fixed top-0 left-0 w-full flex items-center p-3 bg-white border-b border-gray-300 font-sans z-10">
-      {/* โลโก้ */}
+
       <div className="flex items-center mr-10">
         <Image
-          src="/wellwave.svg" // ใช้ path ที่สัมพันธ์กับโฟลเดอร์ public
+          src="/wellwave.svg"
           alt="Logo"
           width={50}
           height={50}
@@ -21,7 +21,7 @@ const Navbar: React.FC = () => {
         />
       </div>
 
-      {/* เมนู ชิดซ้าย */}
+
       <ul className="flex space-x-6 justify-start flex-grow">
         <li>
           <Link
@@ -43,18 +43,17 @@ const Navbar: React.FC = () => {
         </li>
       </ul>
 
-      {/* ปุ่ม Logout */}
       <button
         className="px-6 py-2 text-sm font-semi bg-white text-gray-500 border-2 border-gray-500 rounded-lg hover:bg-red-600 hover:text-white flex items-center justify-between"
         onClick={() => alert('Logged out')}
       >
         <span>ออกจากระบบ</span>
         <Image
-          src="/asset/out.svg" // ใช้ path ที่สัมพันธ์กับโฟลเดอร์ public
+          src="/asset/out.svg"
           alt="fire"
           width={18}
           height={24}
-          className="ml-3" // ให้ไอคอนมีระยะห่างจากข้อความ
+          className="ml-3"
         />
       </button>
     </nav>
